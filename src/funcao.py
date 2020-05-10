807ca4e1879150d9eebf266f5922633e12656bd8
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-05-10 13:10:22 -0300

    criei um main pra fazer apenas a funcao que eu mandar

--- src/funcao.py
+++ src/funcao.py
@@ -1,5 +1,9 @@
 def quadrado(x):
     return x * x
 
-for i in range (10):
-    print("{} ao quadrado é {}".format(i, quadrado(i)))
+def main():
+    for i in range (10):
+        print("{} ao quadrado é {}".format(i, quadrado(i)))
+
+if __name__ == "__main__":
+    main()
